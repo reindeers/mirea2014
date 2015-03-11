a991ebcf2e31994b8d29d258253c35ffa425d236
reindeers <velokarri@mail.ru>
2015-03-11 16:06:13 +0300

    Refactor GoF

---
+++ src/PointPerimeter.java
@@ -0,0 +1,13 @@
+/**
+ * Created by student on 11.03.2015.
+ */
+public class PointPerimeter {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static double perimeter(Point[] points){
+        
+    }
+}
