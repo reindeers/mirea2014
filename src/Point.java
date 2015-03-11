e7059c8a16aa8c37b8bae72cdea8af6259052ccf
reindeers <velokarri@mail.ru>
2015-03-11 16:06:40 +0300

    Refactor GoF

---
+++ src/Point.java
@@ -0,0 +1,27 @@
+/**
+ * Created by student on 11.03.2015.
+ */
+public class Point {
+
+private double x, y;
+
+    Point(double х, double у) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX(){
+        return this.x;
+    }
+
+    public double getY(){
+        return this.y;
+    }
+
+    private double multipleP(){
+        return this.getX()*this.getY();
+    }
+
+
+
+}
