b8dc4b9641a9eb6911829cc6fcb9858d09e43a27
reindeers <velokarri@mail.ru>
2015-03-11 15:46:31 +0300

    Refactor GoF

--- src/GoL_4.java
+++ src/GoL_4.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Created by sever on 22.02.2015.
  *
@@ -9,32 +11,42 @@
 
   */
 public class GoL_4 {
+
+    static final int N = 20;
     public static void main(String[] args) {
 
-        boolean[][] mask2 = {{true, true, true, true},{true, false, false, false},{true, true, false, true}, {true, false, false, true}};
-        boolean[][] mask = new boolean[4][4];
+        boolean[][] mask = new boolean[N][N]; //{{true, true, true, true},{true, false, false, false},{true, true, false, true}, {true, false, false, true}};
+        mask[3][1] = true;
+        mask[3][2] = true;
+        mask[3][3] = true;
+        mask[2][3] = true;
+        mask[1][2] = true;
+//        Random rnd = new Random();
+//        for (int i = 0; i < 20; i++) {
+//            int x = rnd.nextInt(N);
+//            int y = rnd.nextInt(N);
+//            mask[x][y] = true;
+//        }
         Display d = new Display();
 
-        while (1==1){
+        while (true){
             d.setMask(mask);
-            System.arraycopy(mask2, 0, mask, 0, mask2.length);
-            LifeGameSimple(mask, mask2); //ToDO: проверка /* Самая простая версия: простой перебор соседних клеток. */
+            boolean[][] newMask = new boolean[N][N];
+            LifeGameSimple(mask, newMask); //ToDO: проверка /* Самая простая версия: простой перебор соседних клеток. */
+            mask = newMask;
         }
 
     }
 
     private static void LifeGameSimple(boolean[][]mask, boolean[][]mask2){
-        int count1 = 0;
         for (int i = 0; i < (mask.length); i++) {
             for (int j = 0; j < (mask.length); j++) {
-
+                int count1 = countTrue(mask, i, j);
                 if (mask[i][j]) {
-                    count1 = countTrue(mask, i, j);
                     if (count1 < 2) mask2[i][j] = false;
                     if ((count1 == 2) || (count1 == 3)) mask2[i][j] = true;
                     if (count1 > 3) mask2[i][j] = false;
-                } else if (!mask[i][j]) {
-                    count1 = countTrue(mask, i, j);
+                } else {
                     if (count1 == 3) mask2[i][j] = true;
                 }
 
@@ -47,10 +59,10 @@ public class GoL_4 {
         int xM, xP;
         int yM, yP;
 
-        if (i == 0) xM = 3; else xM = i - 1;
-        if (i == 3) xP = 0; else xP = i + 1;
-        if (j == 0) yM = 3; else yM = j - 1;
-        if (j == 3) yP = 0; else yP = j + 1;
+        if (i == 0) xM = N - 1; else xM = i - 1;
+        if (i == N - 1) xP = 0; else xP = i + 1;
+        if (j == 0) yM = N - 1; else yM = j - 1;
+        if (j == N - 1) yP = 0; else yP = j + 1;
 
         if (mask[i][yP]) cnt++;
         if (mask[i][yM]) cnt++;
