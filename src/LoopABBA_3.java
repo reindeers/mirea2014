cda622b33fb4349e4b423139e3e82ca703f3ead2
reindeers <velokarri@mail.ru>
2015-03-18 17:11:28 +0300

    Update LoopABBA_3.java
    Fix "if".

--- src/LoopABBA_3.java
+++ src/LoopABBA_3.java
@@ -2,23 +2,22 @@
  * Created by m.vlasova on 19.02.2015.
  *
  Для целых чисел от 1 до 100 вывести: если число делится на 5 и на 7, вывести строку "ABBA";
-                                      если число делится на 5, вывести строку "A";
-                                      если число делится на 7, вывести строку "B";
-                                      иначе вывести само число.
-
+ если число делится на 5, вывести строку "A";
+ если число делится на 7, вывести строку "B";
+ иначе вывести само число.
  */
 public class LoopABBA_3 {
     public static void main(String[] args) {
-            printmethod();
+        printmethod();
     }
-
+    
     private static void printmethod(){
         for (int i = 1; i <= 100; i++){
-            if  ((Math.round(i % 5) == (i % 5)) & Math.round(i % 7) == (i % 7)){
+            if (((i % 5) == 0 ) & ((i % 7) == 0 )){
                 System.out.println("ABBA");
-            } else if (Math.round(i % 5) == (i % 5)){
+            } else if ((i % 5) == 0 ){
                 System.out.println("A");
-            } else if (Math.round(i % 7) == (i % 7)){
+            } else if ((i % 7) == 0 ){
                 System.out.println("B");
             } else {
                 System.out.print(i);
