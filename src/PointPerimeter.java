1503478659b1c5d763246e6edbbfb8470c74237b
reindeers <velokarri@mail.ru>
2015-03-18 17:33:56 +0300

    Update PointPerimeter.java
    Add if-else.

--- src/PointPerimeter.java
+++ src/PointPerimeter.java
@@ -2,29 +2,30 @@
  * Created by m.vlasova on 12.03.2015.
  */
 
-    public class PointPerimeter {
-        public static void main(String[] args) {
-            Point a = new Point(1, 2);
-            Point b = new Point(3, 4);
-            Point c = new Point(2, 1);
+public class PointPerimeter {
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(3, 0);
+        Point c = new Point(0, 4);
 
-            Point[] points = {a, b, c};
+        Point[] points = {a, b, c};
 
-            System.out.print(perimeter(points));
+        System.out.print(perimeter(points));
 
-        }
-
-        private static double dist(Point a, Point b){
-             return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
-        }
-
-        public static double perimeter(Point[] points){
-            double result = 0;
-            for (int i=0; i < points.length-1; i++){
-                result = result + dist(points[i], points[i+1]);
-            }
-
-            return result;
-        }
     }
 
+    private static double dist(Point a, Point b){
+        return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+    }
+
+    public static double perimeter(Point[] points){
+        double result = 0;
+        for (int i=0; i < points.length; i++){
+            if (i == points.length-1) result = result + dist(points[i], points[0]);
+            else result = result + dist(points[i], points[i+1]);
+
+        }
+
+        return result;
+    }
+}
