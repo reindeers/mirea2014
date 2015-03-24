65e270a89ac005d8dd9eaf9c8157cd0a02341962
reindeers <velokarri@mail.ru>
2015-03-24 15:04:56 +0300

    Update HashMap.java
    Add constructor, equals, remove (need fix).

--- src/HashMap.java
+++ src/HashMap.java
@@ -1,73 +1,64 @@
 import java.util.Objects;
 import java.util.Map;
-/**
- * Created by sever on 22.03.2015.
- */
-public class HashMap<K, V> implements MyMap<K, V> { // ToDo: добавить хэширование с цепочками
 
+public class HashMap<K, V> implements MyMap<K, V> { // ToDo: проверить существующее хэшированние
     private int cnt = 0;
     private Node<K, V>[] ar;
 
-
     HashMap(){
-        ar = (Node<K,V>[])new Node[1000]; //ToDo: fix
+        ar = (Node<K,V>[])new Node[1000]; //ToDo: fix - ?
     }
 
-    static final int hash(Object key) {
+    HashMap(int sz){
+        ar = (Node<K,V>[])new Node[sz];
+    }
+    public int hash(Object key) {
         int h;
         return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16); //ToDo: алгоритм
     }
-
     public void put(K key, V value){
-        int t = hash(key);
-        ar[t] = new Node<K, V>(key, value);
+        ar[hash(key)] = new Node<K, V>(key, value); //ToDo: добавить хэширование с цепочками, проверка флага
         cnt++;
     }
-
     public V get(K key){
-        return ar[hash(key)].getValue();
+        if (ar[hash(key)] != null | ar[hash(key)].isDeletedFlag()) return ar[hash(key)].getValue();
+        else return null;
     }
-
     public void remove(K key){
-        ar[hash(key)] = null; //ToDo: fix
+        if (!ar[hash(key)].isDeletedFlag()) { ar[hash(key)].deletedFlag = false; cnt--; } //ToDO: все равно выдает значение
     }
 
-    public int hash(){
-        return 0; //ToDo: fix
-    }
     public int size(){
         return cnt;
     }
-
     static class Node<K, V> implements Map.Entry<K,V>{
         final int hash = 0;
         final K key;
+        boolean deletedFlag = false;
         V value;
 
-        Node( K key, V value) { //int hash, Node<K,V> next
-            // this.hash = hash;
+        Node( K key, V value) {
             this.key = key;
             this.value = value;
-            // this.next = next;
         }
         public K getKey(){ return key; }
         public V getValue(){return value; }
         public final String toString() { return key + "=" + value; }
-
         public final int hashCode() {
             return Objects.hashCode(key) ^ Objects.hashCode(value);
         } //ToDo: ?
-
         public final V setValue(V newValue) {
             V oldValue = value;
             value = newValue;
             return oldValue;
         }
-
-        public final boolean equals(Object o) {
-            return false; //ToDo: fix
+        public boolean isDeletedFlag(){
+            if (deletedFlag == false) return false;
+            else return true;
+        }
+        public final boolean equals(Node n) {
+            if (this.key == n.key && this.value == n.value) return true;
+            else return false;
         }
     }
-
 }
-
