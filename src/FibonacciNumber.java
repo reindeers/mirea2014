500ef777e73afedfb2f47f4f070f23c4993a99c8
reindeers <velokarri@mail.ru>
2015-05-12 17:05:42 +0300

    Update FibonacciNumber.java

--- src/FibonacciNumber.java
+++ src/FibonacciNumber.java
@@ -1,27 +1,24 @@
 import java.math.BigInteger;
+import java.util.Iterator;
 
 /**
  * Created by sever on 22.03.2015.
  */
-public class FibonacciNumber implements Iterator {
-    private int c = 0;
-    private BigInteger pcnt1 = new BigInteger("0");
-    private BigInteger pcnt2 = new BigInteger("1");
-    private BigInteger cnt = new BigInteger("0");
+public class FibonacciNumber implements Iterator<BigInteger> {
+    private BigInteger fb0 = new BigInteger("0");
+    private BigInteger fb1 = new BigInteger("1");
     public BigInteger next(){
-        if (c == 0) {c++; return cnt;}
-        else if (c == 1) {c++; cnt = new BigInteger("1"); return cnt;}
-        else {
-            cnt = pcnt1.add(pcnt2);
-            pcnt1 = pcnt2;
-            pcnt2 = cnt;
-            c++;
-            return cnt;
-        }
+            BigInteger tmp  = fb0;
+            fb0 = fb1;
+            fb1 = fb1.add(tmp);
+            return fb0;
+
 
     }
+
 
     public boolean hasNext(){
         return true;
     }
+    public void remove(){ }
 }
