e0f62147e8a90f3015770506b0c479df3d03ab85
reindeers <velokarri@mail.ru>
2015-05-19 16:52:20 +0300

    Update TestForIterator.java

--- src/TestForIterator.java
+++ src/TestForIterator.java
@@ -1,4 +1,3 @@
-
 /**
  * Created by sever on 22.03.2015.
  */
@@ -19,13 +18,29 @@ public class TestForIterator {
 
 
         System.out.println("Цикл for-each:");
-        int stop = 100;
+       /* int stop = 100;
         int cnt = 0;
         for (NaturalNumber i = new NaturalNumber(); i.hasNext(); ){
             System.out.println(i.next());
             cnt++;
             if (cnt==stop) break;
 
+        }*/
+
+        NaturalNumberItrbl nn = new  NaturalNumberItrbl();
+        int cnt1 = 0;
+        for (Object i : nn) {
+            System.out.println(i);
+            cnt1++;
+            if (cnt1==20) break;
+        }
+
+        FibonacciNumberItrbl ff = new FibonacciNumberItrbl();
+        int cnt2 = 0;
+        for (Object i : ff) {
+            System.out.println(i);
+            cnt2++;
+            if (cnt2==20) break;
         }
 
     }
